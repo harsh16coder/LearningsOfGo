ae0400ff31d0ef3155a0511ba4d4cfc0fdd4bad7
Your Name <harshgharat663@gmail.com>
2025-07-05 18:14:12 +0530

    Implemnetion of io.writer and logging write calls

--- mocking/main.go
+++ mocking/main.go
@@ -3,16 +3,39 @@ package main
 import (
 	"fmt"
 	"io"
-	"os"
+	"time"
 )
 
-func Countdown(out io.Writer) {
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+const write = "write"
+const sleep = "sleep"
+
+type SpyCountDownOperation struct {
+	Calls []string
+}
+
+func (s *SpyCountDownOperation) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountDownOperation) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+func Countdown(out io.Writer, sleeper *SpyCountDownOperation) {
 	for i := 3; i > 0; i-- {
 		fmt.Fprintln(out, i)
+		sleeper.Sleep()
 	}
 	fmt.Fprintf(out, "Go!")
 }
 
 func main() {
-	Countdown(os.Stdout)
+	// Countdown(os.Stdout,)
 }
--- mocking/mocking_test.go
+++ mocking/mocking_test.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
 func TestGreet(t *testing.T) {
 	t.Run("Greet", func(t *testing.T) {
 		buffer := &bytes.Buffer{}
-		Countdown(buffer)
+		Countdown(buffer, &SpyCountDownOperation{})
 		got := buffer.String()
 		want := `3
 2
@@ -16,6 +17,25 @@ func TestGreet(t *testing.T) {
 Go!`
 		if got != want {
 			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+	t.Run("sleep before every print", func(t *testing.T) {
+		spySleepPrinter := &SpyCountDownOperation{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		want := []string{
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
 		}
 	})
 }
