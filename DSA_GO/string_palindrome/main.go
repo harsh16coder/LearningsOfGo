12b630e0a22701e3679d2a2b783003186d3db9e8
Your Name <harshgharat663@gmail.com>
2025-11-11 22:48:08 +0530

    string palidrome

---
+++ DSA_GO/string_palindrome/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"unicode"
+)
+
+func palindrome(s string) bool {
+	l, r := 0, len(s)-1
+
+	for l < r {
+		for l < r && !isAlphaNum(rune(s[l])) {
+			l++
+		}
+		for r > l && !isAlphaNum(rune(s[r])) {
+			r--
+		}
+		if unicode.ToLower(rune(s[l])) != unicode.ToLower(rune(s[r])) {
+			return false
+		}
+		l++
+		r--
+	}
+	return true
+}
+func isAlphaNum(c rune) bool {
+	return unicode.IsLetter(c) || unicode.IsDigit(c)
+}
+func main() {
+	str := "Was it a car or a cat I saw"
+	fmt.Println(palindrome(str))
+}
